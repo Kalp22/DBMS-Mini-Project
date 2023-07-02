f6daab342570db041ed4b6e7e745982a8707ec02
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-07-03 00:59:23 +0530

    Update

---
+++ api/Criminals.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const client = new MongoClient(process.env.DB_ADD);
+
+router.get('/',async (req,res)=>{
+   try {
+      await client.connect();
+      const db = client.db('MiniProjectDBMS');
+      const collectionArray = await db.collection('CriminalDatabase').aggregate().toArray();
+   } catch (err) {
+      console.log(err)
+   }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const PORT = process.env.PORT
-
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
+
+// login route
+const login = require('./api/login.js')
+// Display top 10 Criminals route
+// const topCriminals = require('./api/topCriminals.js')
+// Display all Criminals route
+const criminals = require('./api/Criminals.js');
+// Add more Criminals route
+// const addCriminal = require('./api/addCriminal.js');
+
+// using the routes
+app.use('/login',login)
+// app.use('/topCriminals',topCriminals);
+app.use('/criminals',criminals);
+// app.use('/addCriminal',addCriminal);
 
 app.listen(PORT, () => {
    console.log(`listening on ${PORT}`);
